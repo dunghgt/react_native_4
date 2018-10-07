02a81b280cff8cec312e7a241e3b95967d5efe76
dunghgt <33288571+dunghgt@users.noreply.github.com>
2018-10-08 00:55:30 +0700

    Update MovieIcon.js

--- reduxAndAPI/src/components/MovieIcon.js
+++ reduxAndAPI/src/components/MovieIcon.js
@@ -42,30 +42,6 @@ const styles = StyleSheet.create({
         elevation: 10,
         justifyContent: 'center',
     },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: '#f2f2f2',
-        marginBottom: 6,
-    },
-    vote: {
-        fontSize: 18,
-        color: '#ff8000',
-    },
-    textvote: {
-        fontSize: 15,
-        color: '#e6e6e6'
-    },
-    release: {
-        fontSize: 15,
-        fontWeight: '600',
-        color: '#f2f2f2'
-    },
-    releasedate: {
-        fontSize: 14,
-        color: '#e6e6e6',
-        marginLeft: 5
-    }
 })
 
 export default MovieIcon;
