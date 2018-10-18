8366f47c2f093c6bbeac1e0ed7678228398a1752
dunghgt <dangtothedung@gmail.com>
2018-10-19 02:21:30 +0700

    show vector-icon

--- shopping/android/app/src/main/java/com/orderfood/MainApplication.java
+++ shopping/android/app/src/main/java/com/orderfood/MainApplication.java
@@ -1,4 +1,4 @@
-package com.orderfood;
+package com.shopping;
 
 import android.app.Application;
 
--- shopping/ios/shoppingTests/shoppingTests.m
+++ shopping/ios/shoppingTests/shoppingTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface OrderFoodTests : XCTestCase
+@interface shoppingTests : XCTestCase
 
 @end
 
-@implementation OrderFoodTests
+@implementation shoppingTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
